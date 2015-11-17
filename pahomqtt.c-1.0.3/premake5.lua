c2a12d3d058930c7168e0db40d417d176d7f93be
Standil <cpascal@cpascal.net>
2015-11-18 01:13:28 +0900

    First upload of premake file.

---
+++ pahomqtt.c-1.0.3/premake5.lua
@@ -0,0 +1,48 @@
+-- premake5.lua
+require "../premake/modules/ndk/ndk"
+
+workspace "pahomqtt"
+    configurations { "Debug", "Release" }
+    platforms { "android" }
+
+project "pahomqtt"
+    language "C"
+    kind "StaticLib"
+    targetdir "build/%{cfg.buildcfg}"
+    files {
+        "src/Clients.c",
+        "src/Heap.c",
+        "src/LinkedList.c",
+        "src/Log.c",
+        "src/Messages.c",
+        "src/MQTTPacket.c",
+        "src/MQTTPacketOut.c",
+        "src/MQTTPersistence.c",
+        "src/MQTTPersistenceDefault.c",
+        "src/MQTTProtocolClient.c",
+        "src/MQTTProtocolOut.c",
+        "src/Socket.c",
+        "src/SocketBuffer.c",
+        "src/SSLSocket.c",
+        "src/StackTrace.c",
+        "src/Thread.c",
+        "src/Tree.c",
+        "src/utf-8.c"
+    }
+
+    filter "configurations:Debug"
+        defines { "DEBUG", "_DEBUG" }
+        flags { "Symbols" }
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+
+    filter { "configurations:vs*", "language:C or C++" }
+        defines { "WIN32" }
+
+    filter { "platforms:android" }
+        defines { "WIN32" }
+
+    filter {}
+
